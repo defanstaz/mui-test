e205210d678a382403c68a39bb0bfed40d2d0110
warrenmercer <wmercer@stanford.edu>
2022-01-20 09:14:57 -0500

    dev v0.1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Demo from './demo';
+import Map from './map';
 
 ReactDOM.render(
   <StyledEngineProvider injectFirst>
     <Demo />
   </StyledEngineProvider>,
   document.querySelector("#root")
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Map />
+  </React.StrictMode>,
+  document.querySelector("#map")
 );
